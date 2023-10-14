2988d068d0e40ee4c9caccd83b9517a3e627bfab
ng-tjarwan <tareq.jarwan@bitoasis.net>
2023-10-14 03:21:38 +0300

    variants and search

--- src/app/components/DateSelection.jsx
+++ src/app/components/DateSelection.jsx
@@ -1,3 +1,62 @@
+"use client";
+
+import { Menu } from "@headlessui/react";
+import { useState } from "react";
+
+// icons
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+// React Date
+import { DateRange } from "react-date-range";
+import { format, addDays } from "date-fns";
+
+// react data range css
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
 export default function DateSelection() {
-  return <div>About</div>;
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+  return (
+    <Menu as="div" className="w-full h-full flex xl:flex-row ">
+      <div className="relative flex-1">
+        {/* btn */}
+        <Menu.Button className="dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
+          <div className="flex flex-col xl:flex-row items-center gap-y-2 xl:gap-x-2 xl:gap-y-0">
+            <FaCalendarAlt className="text-accent" />
+            <div className="text-[15px] uppercase font-bold">Select Date</div>
+          </div>
+          <div className="flex items-center gap-x-3 xl:ml-6">
+            <div className="text-[13px] font-medium text-secondary">
+              {format(date[0].startDate, "dd/MM/yyyy")}
+            </div>
+            <FaArrowRightLong className="text-accent text-[12px]" />
+            <div className="text-[13px] font-medium text-secondary">
+              {format(
+                date[0].endDate ? date[0].endDate : date[0].startDate,
+                "dd/MM/yyyy"
+              )}
+            </div>
+          </div>
+        </Menu.Button>
+        {/* menu */}
+        <Menu.Items className="dropdown-menu shadow-lg absolute -top-96 xl:top-[90px] left-1/2 xl:left-0 z-50 transform -translate-x-1/2 xl:-translate-x-0 rounded-[10px] overflow-hidden">
+          <DateRange
+            editableDateInputs={true}
+            moveRangeOnFirstSelection={false}
+            ranges={date}
+            onChange={(item) => setDate([item.selection])}
+            rangeColors={["#ed1d24"]}
+            minDate={addDays(new Date(), 0)}
+          />
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
 }
--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -3,6 +3,8 @@
 import { useContext } from "react";
 import { SearchContext } from "../context/Search";
 import LocationSelection from "./LocationSelection";
+import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 export default function Search() {
   const { searchActive } = useContext(SearchContext);
@@ -12,11 +14,27 @@ export default function Search() {
       className={`${
         searchActive
           ? "rounded-none xl:h-[80px]"
-          : "rounded-[20px] py-6 xl:h-[98px]"
+          : "rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
       } bg-white hidden xl:block w-full relative shadow-lg`}
     >
-      <div className="xl:h-full flex items-center px-6 xl:px-0">
+      <div
+        className={`${
+          searchActive && "container mx-auto"
+        } h-full flex items-center px-6 xl:px-0`}
+      >
         <LocationSelection />
+        <DateSelection />
+        <HoursSelection />
+        {/* btn */}
+        <div className="xl:h-full flex items-center px-6 xl:px-0">
+          <button
+            className={`${
+              searchActive ? "btn-sm xl:w-[164px]" : "btn-lg xl:w-[184px]"
+            } btn btn-accent`}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
