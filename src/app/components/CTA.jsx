d151abd8629215da5fbc1584537b6c607f5c799f
ng-tjarwan <tareq.jarwan@bitoasis.net>
2023-10-15 22:33:05 +0300

    cta

--- src/app/components/CTA.jsx
+++ src/app/components/CTA.jsx
@@ -1,10 +1,90 @@
-export default function CTA() {
+"use client";
+
+// next image
+import Image from "next/image";
+
+// motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../../../variants";
+
+export default function Cta() {
   return (
     <section
-      id="contact"
       className="pt-24 xl:pt-48 pb-0 flex items-center bg-[#b2b7c2]/10"
+      id="contact"
     >
-      Cars
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row xl:items-center">
+          {/* text */}
+          <div className="flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0">
+            <div className="max-w-[520px] mx-auto order-2 xl:order-none">
+              <motion.h2
+                variants={fadeIn("right", 0.2)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.6 }}
+                className="h2"
+              >
+                Download our App now and hit the road with ease
+              </motion.h2>
+              <motion.p
+                variants={fadeIn("right", 0.4)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.6 }}
+                className="mb-10"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident eos, dicta maiores quas nisi quos iusto excepturi,
+                ipsa saepe tempore nostrum deleniti doloremque praesentium
+                veniam, earum aspernatur eveniet numquam minima.
+              </motion.p>
+              {/* btns */}
+              <motion.div
+                variants={fadeIn("right", 0.6)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.6 }}
+                className="flex gap-x-3 justify-center md:justify-start"
+              >
+                <button className="btn-cta">
+                  <Image
+                    src={"/icons/buttons/google-play.svg"}
+                    alt=""
+                    width={132}
+                    height={36}
+                  />
+                </button>
+                <button className="btn-cta">
+                  <Image
+                    src={"/icons/buttons/app-store.svg"}
+                    alt=""
+                    width={132}
+                    height={36}
+                  />
+                </button>
+              </motion.div>
+            </div>
+          </div>
+          {/* image */}
+          <motion.div
+            variants={fadeIn("up", 0.8)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.2 }}
+            className="flex-1 flex justify-center order-1 md:order-none"
+          >
+            <Image
+              src="/images/cta/phone.svg"
+              alt=""
+              width={320}
+              height={640}
+            />
+          </motion.div>
+        </div>
+      </div>
     </section>
   );
 }
